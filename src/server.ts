902b7fa9b00862651d5a993b773540e8b385e919
Hackante <kante.hagen@gmail.com>
2022-06-14 22:07:45 +0200

    server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,7 @@
 console.log("Booting bot...");
 require("dotenv").config();
 
-var fs = require("fs");
-var db = require("quick.db");
-
-const Sentry = require("@sentry/node");
-const Tracing = require("@sentry/tracing");
+import db = require("quick.db");
 
 if(db.get("emergencystop")) {
     setTimeout(() => {
@@ -13,10 +9,3 @@ if(db.get("emergencystop")) {
         process.exit(0);
     })
 }
-
-const mongo = require("./db.ts")
-var { Client } = require("discord.js")
-const client = new Client({ intents: ["GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "DIRECT_MESSAGES", "GUILDS", "GUILD_MEMBERS", "GUILD_BANS", "GUILD_EMOJIS_AND_STICKERS", "GUILD_PRESENCES"] })
-client.db = db
-client.dbs = mongo
-client.Sentry = Sentry

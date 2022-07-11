c4cb2a8880880430c78ce454387699b54b666bd2
Hackante <kante.hagen@gmail.com>
2022-07-11 12:16:24 +0200

    version command

---
+++ src/commands/version.ts
@@ -0,0 +1,40 @@
+import { CommandInteraction } from "discord.js";
+import Command = require("../config/classes/command");
+import { ExtendedClient } from "../server";
+
+let command = new Command({
+    commandObject: {
+        name: "version",
+        description: "Get all information about the bot's current version",
+    },
+    run: async (interaction: CommandInteraction, client: ExtendedClient) => {
+        // Get the version of the bot
+        let version = {
+            branch: client.info.branch,
+            commit: client.info.commit,
+            uptime: client.uptime,
+            version: require(process.cwd() + "/package.json").version,
+            // get the current commit message
+            commitMessage: require("child_process").execSync("git log -1 --pretty=%B").toString().trim(),
+        }
+
+        // Create an embed with the version information
+        let embed = {
+            title: "Version Information",
+            fields: [
+                { name: "Version", value: version.version },
+                { name: "Branch", value: version.branch },
+                { name: "Commit (use this for bug reports)", value: version.commit},
+                { name: "Commit Message", value: version.commitMessage },
+                { name: "Uptime", value: `<t:${((Date.now() - client.uptime)/1000).toFixed()}:R>` },                
+            ],
+            timestamp: new Date(),
+            color: 5793266 // Blurple
+        }
+        interaction.reply({ embeds: [embed] });
+    }
+});
+
+
+
+module.exports = command;
